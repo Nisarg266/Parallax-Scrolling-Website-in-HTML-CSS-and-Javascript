a488d4b7a14eb6f353472ec16f8247190a7212d7
Nisarg Panchal <142592097+Nisarg266@users.noreply.github.com>
2024-12-27 00:13:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let pyramid = document.getElementById("pyramid");
+let text = document.getElementById("text");
+let stone = document.getElementById("stone");
+let man = document.getElementById("man");
+
+window.addEventListener("scroll",function(){
+    let value = window.scrollY;
+    pyramid.style.right = value * 0.25 + 'px';
+    text.style.left = value * 2 + 'px';
+    stone.style.right = value * 1 + 'px';
+    man.style.left = value * 0.5 + 'px';
+})
